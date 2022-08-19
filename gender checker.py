1862aa40f3487460a0b0ffc885a12c6e839f5430
RhysRoo <93945223+RhysRoo@users.noreply.github.com>
2022-08-19 16:00:55 +0100

    Add files via upload

---
+++ gender checker.py
@@ -0,0 +1,13 @@
+def what_is_your_gender(gender,sex):
+    if gender == True and sex == True:
+        return print("Your male")
+    elif gender == True and sex == False:
+        return print("Your a transgender man")
+    elif gender == False and sex == True:
+        return print("Your a transgender woman")
+    elif gender == False and sex == False:
+        return print("Your female")
+    else:
+        return print("Error")
+
+what_is_your_gender(True,True)
